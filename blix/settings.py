982958cd1dfbb6c5cbf790e8b442acc140ede9c1
Connor McBride <connor@ubuntu.(none)>
2014-05-28 17:56:49 -0500

    Added Database info

--- blix/settings.py
+++ blix/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'blix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blix_db',
+        'USER': 'django_login',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
